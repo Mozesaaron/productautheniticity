381e41a0eb29d0976733272677564e8d873f6c7f
moses <mozesmpanga@gmail.com>
2024-02-23 13:01:08 +0300

    fourth commit

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ from flask import Flask , render_template , request , jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def addnumbers():
+def productauthenticity():
     return render_template('index.html')
-
-@app.route('/getthesum',  methods=['POST'])
-def thesumresult():
-        data = request.get_json()
-        num1 = int(data.get('num1'))
-        num2 = int(data.get('num2'))
-        result = num1 + num2
-        return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
