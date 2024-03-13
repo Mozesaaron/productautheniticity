1d9b410c2c8441e6a8e9e802dd04a1faf19a3d8a
moses <mozesmpanga@gmail.com>
2024-03-13 04:08:44 +0300

    login page and dashboard

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def productauthenticity():
     return render_template('index.html')
 
+@app.route('/login')
+def displaytheloginpage():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def distributorsdb():
+    return render_template('distributorsdb.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
 
